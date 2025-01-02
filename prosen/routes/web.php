447b6cc3aa1bd2a661ffe7c15a4065396eb74ce6
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-02 17:31:52 +0600

    1/2/25

--- prosen/routes/web.php
+++ prosen/routes/web.php
@@ -1,13 +1,57 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Translation\ArrayLoader;
 
 Route::get('/', function () {
     return view('home');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/jobs', function () {
+    return view('jobs', [
+        'jobs' => [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$150,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programer',
+                'salary' => '$100,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$50,000',
+            ]
+        ]
+    ]);
+});
+
+Route::get('/jobs/{id}', function ($id) {
+    $jobs = [
+            [
+                'id' => 1,
+                'title' => 'Director',
+                'salary' => '$150,000',
+            ],
+            [
+                'id' => 2,
+                'title' => 'Programer',
+                'salary' => '$100,000',
+            ],
+            [
+                'id' => 3,
+                'title' => 'Teacher',
+                'salary' => '$50,000',
+            ]
+        ];
+
+        $job =Arr::first($jobs,fn($job) => $job['id']==$id);
+        
+        return view('job' ,['job' => $job]);
 });
 
 Route::get('/contact', function () {
