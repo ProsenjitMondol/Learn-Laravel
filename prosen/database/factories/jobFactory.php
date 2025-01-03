7f2fdc6590ae1e8bdd90995d410d8b432491ee3a
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-03 16:45:50 +0600

    1/3/24

---
+++ prosen/database/factories/jobFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+
+use Faker\Generator as Faker;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\job>
+ */
+class jobFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => $this->faker->jobTitle(),
+            'salary' => '$500,000 USD'
+        ];
+    }
+}
