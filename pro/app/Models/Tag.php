6b0bda1c8a018ed75a85ae994b118356d46a4100
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-08 00:19:16 +0600

    1/8/25

--- pro/app/Models/Tag.php
+++ pro/app/Models/Tag.php
@@ -7,13 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 
+
 class Tag extends Model
 {
     /** @use HasFactory<\Database\Factories\TagFactory> */
     use HasFactory;
+    
 
     public function jobs()
     {
-        return $this->belongsToMany(Job::class,'job_tag', 'tag_id',"job_listing_id");
+        return $this->belongsToMany(Job::class,relatedPivotKey:'job_listing_id');
     }
 }
