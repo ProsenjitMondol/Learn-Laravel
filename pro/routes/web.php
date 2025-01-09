1b5cdc92cfd78a96aa07499af9e8d31cf10a709e
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-10 02:35:31 +0600

    1/10/25

--- pro/routes/web.php
+++ pro/routes/web.php
@@ -12,10 +12,10 @@ Route::get('/', function () {
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::all();
+    $jobs = Job::with('employer')->get();
     
     return view('jobs', [
-        'jobs' => Job::all()
+        'jobs' => $jobs
     ]);
 });
 
