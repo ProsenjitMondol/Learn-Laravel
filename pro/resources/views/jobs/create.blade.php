5edc16b65b4affeaf555efa24926223aaebcc68c
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-14 01:51:08 +0600

    1/14/25

--- pro/resources/views/jobs/create.blade.php
+++ pro/resources/views/jobs/create.blade.php
@@ -30,6 +30,19 @@
           </div>
         </div>
        </div>
+
+       <div class="mt-10">
+
+       @if($errors->any())
+        <ul>
+        @foreach($errors->all() as $error)
+        <li class="text-red-500 italic">{{$error}}</li>
+        @endforeach
+        </ul>
+
+        @endif
+       </div>
+
     </div>
   </div>
 
