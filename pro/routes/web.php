4d903e31a43462c99b98b93a016b542785ac9df4
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-24 16:22:53 +0600

    1/24/25

--- pro/routes/web.php
+++ pro/routes/web.php
@@ -3,29 +3,27 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Translation\ArrayLoader;
-use App\Models\Job;
+//use App\Models\Job;
 
 use App\Http\Controllers\JobController;
 use App\Http\Controllers\RegisteredUserController;
 use App\Http\Controllers\SessionController;
 use App\Jobs\TranslateJob;
+use Illuminate\Types\Relations\Role;
 
 
-Route::get('/', function () {
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
 
-    return view('home');
-});
+// Route::controller(JobController::class)->group(function () {
+//     Route::get('/jobs',  'index');
+//     Route::get('/jobs/create', 'create');
+//     Route::get('/jobs/{job}','show');
+//     Route::post('/jobs',  'store');
+//     Route::get('/jobs/{job}/edit', 'edit');
+//     Route::patch('/jobs/{job}',  'update');
+//     Route::delete('/jobs/{job}', 'destroy');
+// });
 
+Route::resource('jobs',JobController::class);
 
-Route::get('/jobs', [JobController::class, 'index']);                         
-Route::get('/jobs/create',[JobController::class,'create']);
-Route::get('/jobs/{job}', [JobController::class,'show']);
-Route::post('/jobs',[JobController::class,'store']);
-Route::get('/jobs/{job}/edit',[JobController::class,'edit']);
-Route::patch('/jobs/{job}', [JobController::class,'update']);
-Route::delete('/jobs/{job}',[JobController::class,'destroy']);
-
-
-Route::get('/contact', function () {
-    return view('contact');
-});
