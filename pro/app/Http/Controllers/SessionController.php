2475711d2c49463f3bc799241f8e744e95a2bf77
Prosenjit Mondol <prosen1156@gmail.com>
2025-01-30 01:58:55 +0600

    1/30/25

--- pro/app/Http/Controllers/SessionController.php
+++ pro/app/Http/Controllers/SessionController.php
@@ -4,14 +4,24 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class SessionController extends Controller
 {
-    public function create(){
+    public function create()
+    {
         return view('auth.login');
     }
 
-    public function store(){
+    public function store()
+    {
         dd(request()->all());
+    }
+
+    public function destroy()
+    {
+        Auth::logout();
+
+        return redirect('/');
     }
 }
--- pro/resources/views/Components/layout.blade.php
+++ pro/resources/views/Components/layout.blade.php
@@ -28,10 +28,19 @@
         <div class="hidden md:block">
           <div class="ml-4 flex items-center md:ml-6">
 
-                   
+            @guest
             <x-nav-link href="/login" :active="request()->is('login')">Log In</x-nav-link>
             <x-nav-link href="/register" :active="request()->is('register')">Register</x-nav-link>
+            @endguest       
             
+            @auth
+           <form method="post" action="/logout">
+            <x-form-button>
+              @csrf
+              Log Out
+            </x-form-button>
+           </form>
+            @endauth
 
           </div>
         </div>
